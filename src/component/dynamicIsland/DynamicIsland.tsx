2a18c5fd722a222de456c8b13277c2ed97473e9b
Yefree Valdez <yefree13santos@outlook.com>
2023-10-05 19:41:53 +0200

    feat: create base states

--- src/component/dynamicIsland/DynamicIsland.tsx
+++ src/component/dynamicIsland/DynamicIsland.tsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 // import styles
 import "./dynamicIsland.scss";
 
 export default function DynamicIsland() {
+  const [dataMode, setDataMode] = useState("");
+
+  const handleDataMode = (mode: string) => {
+    setDataMode(mode);
+  };
+
   return (
     <header className="header">
-      <div className="island" aria-label="Dynamic island">
+      <div className="island" aria-label="Dynamic island" data-mode={dataMode}>
         <nav className="island__nav">
           <ul className="island__nav_ul">
             <li>
-              <button>state 1</button>
+              <button type="button" onClick={() => handleDataMode("")}>
+                regular
+              </button>
             </li>
             <li>
-              <button>state 2</button>
+              <button type="button" onClick={() => handleDataMode("expanded")}>
+                expanded
+              </button>
             </li>
             <li>
-              <button>state 3</button>
+              <button type="button" onClick={() => handleDataMode("square")}>
+                square
+              </button>
             </li>
             <li>
-              <button>state 4</button>
+              <button type="button" onClick={() => handleDataMode("divide")}>
+                divide
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={() => handleDataMode("large")}>
+                large
+              </button>
             </li>
           </ul>
         </nav>
