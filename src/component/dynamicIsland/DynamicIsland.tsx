90cf8c2b60cb7878800056f47cd4f4ab4bf0b7be
Yefree Valdez <yefree13santos@outlook.com>
2023-10-08 12:29:08 +0200

    feat: expanded items styles and components

--- src/component/dynamicIsland/DynamicIsland.tsx
+++ src/component/dynamicIsland/DynamicIsland.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-// import styles
 import "./dynamicIsland.scss";
 
 import Add from "../icons/icons";
 
 export default function DynamicIsland() {
   const [dataMode, setDataMode] = useState<string | null>(null);
+  // const [showExpandedItems, setShowExpandedItems] = useState<boolean>(false);
 
   const handleDataMode = (event: React.MouseEvent<HTMLButtonElement>) => {
     const dataMode = event.currentTarget.getAttribute("data-mode");
@@ -14,50 +14,47 @@ export default function DynamicIsland() {
 
   return (
     <header className="header">
+      <ul className="island__nav_ul">
+        <li>
+          <button type="button" data-mode={null} onClick={handleDataMode}>
+            regular
+          </button>
+        </li>
+        <li>
+          <button type="button" data-mode="expanded" onClick={handleDataMode}>
+            expanded
+          </button>
+        </li>
+        <li>
+          <button type="button" data-mode="square" onClick={handleDataMode}>
+            square
+          </button>
+        </li>
+        <li>
+          <button type="button" data-mode="divide" onClick={handleDataMode}>
+            divide
+          </button>
+        </li>
+        <li>
+          <button type="button" data-mode="large" onClick={handleDataMode}>
+            large
+          </button>
+        </li>
+      </ul>
       <div className="island" aria-label="Dynamic island" data-mode={dataMode}>
-        {dataMode === "expanded" && (
-          <div className="expanded_items">
-            <Add />
-          </div>
-        )}
+        <div className="expanded_items">
+          <Add />
+        </div>
         <nav className="island__nav">
           <ul className="island__nav_ul">
-            <li>
-              <button type="button" data-mode={null} onClick={handleDataMode}>
-                regular
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                data-mode="expanded"
-                onClick={handleDataMode}
-              >
-                expanded
-              </button>
-            </li>
-            <li>
-              <button type="button" data-mode="square" onClick={handleDataMode}>
-                square
-              </button>
-            </li>
-            <li>
-              <button type="button" data-mode="divide" onClick={handleDataMode}>
-                divide
-              </button>
-            </li>
-            <li>
-              <button type="button" data-mode="large" onClick={handleDataMode}>
-                large
-              </button>
-            </li>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
           </ul>
         </nav>
-        {dataMode === "expanded" && (
-          <div className="expanded_items">
-            <Add />
-          </div>
-        )}
+        <div className="expanded_items">
+          <Add />
+        </div>
       </div>
     </header>
   );
